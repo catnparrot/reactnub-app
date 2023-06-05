969bfc093036d86fd2a44813a9d18401e3712f56
DESKTOP-6FLLH9U\503 <idiotpi3rrot@gmail.com>
2023-06-05 13:24:32 +0900

    toy?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1><a href="/">WEB</a></h1>
       </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">HTML</a></li>
+          <li><a href="/read/2">CSS</a></li>
+          <li><a href="/read/3">Javascript</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
