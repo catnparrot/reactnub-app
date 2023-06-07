95553ae0a033dd899992305e8afa133e0054297c
DESKTOP-6FLLH9U\503 <idiotpi3rrot@gmail.com>
2023-06-07 17:50:09 +0900

    rewind-~p41: comp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function Header() {
+    return(
       <header>
         <h1><a href="/">WEB</a></h1>
       </header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">HTML</a></li>
-          <li><a href="/read/2">CSS</a></li>
-          <li><a href="/read/3">Javascript</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+    )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href="/read/1">HTML</a></li>
+        <li><a href="/read/2">CSS</a></li>
+        <li><a href="/read/3">JavaScript</a></li>
+      </ol>
+    </nav>
+  )
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Article />
     </div>
   );
 }
